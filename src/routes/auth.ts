6a7e04b10da256242947798e30f1e02260117643
angelMarcosCastilla <ajmarcosc15@gmail.com>
2023-02-03 12:02:48 -0500

    add logout in route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import { login } from '../controllers/auth..controller'
+import { login, logout } from '../controllers/auth.controller'
+import { isAutorized } from '../middleware/autorization'
 
 const Router = express.Router()
 
 Router.post('/login', login)
+Router.post('/logout', isAutorized, logout)
 
 export { Router }
